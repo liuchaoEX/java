51f5447612ceab65f54bc87d01faff96a4a9a891
Liuchao <liuchaoex@gmail.com>
2022-11-11 16:45:18 +0800

    Delete Java directory

--- Java/lesson/Demo1.java
+++
@@ -1,7 +0,0 @@
-package lesson;
-
-public class Demo1 {
-    public static void main(String[] args) {
-        System.out.println("点赞又不要钱，投币您看着来。");
-    }
-}
--- Java/planWar1/Play.java
+++
@@ -1,8 +0,0 @@
-package planWar1;
-
-public class Play {
-    public static void main(String[] args) {
-        //创建游戏控制对象
-        Control control = new Control();
-    }
-}
