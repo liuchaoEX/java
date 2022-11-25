c0f0820719577a65e6646328da40b20b0db69954
Liuchao <liuchaoex@gmail.com>
2022-11-25 18:11:23 +0800

    Delete Java/planWar3 directory

--- Java/planWar3/Bullet.java
+++
@@ -1,9 +0,0 @@
-package planWar3;
-
-public interface Bullet extends Display{
-    
-    void move();
-
-    
-    void hurt();
-}
--- Java/planWar3/Display.java
+++
@@ -1,8 +0,0 @@
-package planWar3;
-
-import java.awt.Graphics;
-
-public interface Display {
-    //一个抽象方法，实现绘制的功能
-    void paint(Graphics g);
-}
