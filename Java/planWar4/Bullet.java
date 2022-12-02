86d32213ce1e43ec7207ef898d9ea5196a6c1257
Liuchao <liuchaoex@gmail.com>
2022-12-02 11:49:26 +0800

    Delete Java/planWar4 directory

--- Java/planWar4/Bullet.java
+++
@@ -1,9 +0,0 @@
-package planWar4;
-
-public interface Bullet extends Display{
-    
-    void move();
-
-    
-    void hurt();
-}
--- Java/planWar4/Display.java
+++
@@ -1,8 +0,0 @@
-package planWar4;
-
-import java.awt.Graphics;
-
-public interface Display {
-    //一个抽象方法，实现绘制的功能
-    void paint(Graphics g);
-}
